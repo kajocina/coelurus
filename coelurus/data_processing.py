dd38c5df5d9e3b77294de597b7e403e2d1914dd5
kajocina <kajocina@gmail.com>
2018-07-03 14:28:01 +0200

    added some docstrings

--- coelurus/data_processing.py
+++ coelurus/data_processing.py
@@ -9,19 +9,38 @@ The output from the module is ready to be used by the machine learning part of t
 import pandas as pd
 import ConfigParser
 config = ConfigParser.ConfigParser()
-config.readfp(open('./config.ini','r'))
+config.readfp(open('./config.ini', 'r'))
 
 class Loader():
-    """
+    """ Input data loader.
+
+    This class takes care of data reading and performing basic data quality checks.
 
     """
     def __init__(self):
+        """
+
+        self.data_source reads the config entry in config.ini regarding the source of the data.
+            Entry 'local' means that the file is a resource on a local disk.
+            Entry 'AWS' means that the input data is hosted using the S3 cloud service.
+
+        """
         self.data_source = config.get('data_sources', 'data_source')
+        self.input_path = None
+        self.input_data = None
 
     def load_data(self):
+        """
+
+        Uses pandas to read the input data with profiles and stores as input_data attribute.
+
+        """
         if self.data_source == 'local':
             self.input_path = config.get('data_sources', 'input_data_path')
             self.input_data = pd.read_csv(self.input_path)
         elif self.data_source == 'AWS':
             # import boto3
             pass  # add AWS S3 support using boto3
+
+    def quality_check(self):
+        
