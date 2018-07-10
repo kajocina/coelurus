6ac076bbc1be84a136119ecb1089b892c74f3cd1
kajocina <kajocina@gmail.com>
2018-07-10 15:47:55 +0200

    added filter missing profiles test 2

--- tests/test_DataPrepare.py
+++ tests/test_DataPrepare.py
@@ -62,7 +62,32 @@ def test_imputation2(tmpdir):
     return pd.testing.assert_frame_equal(imputed, expected_result)
 
 
-def test_filter_missing_profiles1(monkeypatch, tmpdir):
+def test_filter_missing_profiles1(tmpdir):
+
+    # create mock config
+    mock_conf = tmpdir.mkdir('mock_config').join('mock_config.ini')
+    mock_conf.write('[data_sources]\ndata_source = local')
+
+    # set up classes and create mock data/config
+    loader = coelurus.Loader(str(tmpdir.join('mock_config', 'mock_config.ini')))
+    loader.config.add_section('filter_options')
+    loader.config.set('filter_options', 'min_consecutive_fractions', '3')
+
+    loader.input_data = pd.DataFrame({'protein_id': ['A', 'B', 'C', 'D', 'E'], 'F1A': [1, 1, 1, 1, 1],
+                                      'F2A': [1, 1, 1, 1, 1], 'F3A': [1, 1, 1, 1, 1],
+                                      'F4A': [1, 1, 1, 1, 1], 'F5A': [1, 1, 1, 1, 1],
+                                      'F6A': [1, 1, 1, 1, 1], 'F7A': [1, 1, 1, 1, 1]},
+                                     columns=['protein_id', 'F1A', 'F2A', 'F3A', 'F4A', 'F5A', 'F6A', 'F7A'])
+
+    expected_result = loader.input_data.copy()  # should be unchanged
+
+    val = coelurus.Validator(loader)
+    dfilter = coelurus.DataPrepare(val)
+    filtered = dfilter.filter_missing_profiles(loader.input_data)
+
+    return pd.testing.assert_frame_equal(filtered, expected_result)
+
+def test_filter_missing_profiles2(tmpdir):
 
     # create mock config
     mock_conf = tmpdir.mkdir('mock_config').join('mock_config.ini')
@@ -79,12 +104,7 @@ def test_filter_missing_profiles1(monkeypatch, tmpdir):
                                       'F6A': [2, 1, 0, 1, 1], 'F7A': [0, 0, 1, 0, 1]},
                                      columns=['protein_id', 'F1A', 'F2A', 'F3A', 'F4A', 'F5A', 'F6A', 'F7A'])
 
-    expected_result = pd.DataFrame({'protein_id': ['A', 'B', 'C', 'D', 'E'], 'F1A': [2, 0, 0, 0, 0],
-                                      'F2A': [1, 1, 1, 0, 0], 'F3A': [1, 0, 2, 1, 0],
-                                      'F4A': [1, 1, 0, 0, 0], 'F5A': [0, 1, 1, 0, 1],
-                                      'F6A': [2, 1, 0, 1, 1], 'F7A': [0, 0, 1, 0, 1]},
-                                     columns=['protein_id', 'F1A', 'F2A', 'F3A', 'F4A', 'F5A', 'F6A', 'F7A'])
-    expected_result = expected_result.iloc[[0, 1, 4], :]  # C and D should be removed only
+    expected_result = loader.input_data.copy().iloc[[0, 1, 4], :]  # C and D should be removed only
 
     val = coelurus.Validator(loader)
     dfilter = coelurus.DataPrepare(val)
